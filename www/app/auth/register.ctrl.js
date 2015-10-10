a6bc4900791734e6227d7b2789291e3acaa484d1
Harshil Shah <hash_1492@yahoo.com>
2015-10-11 01:48:55 +0530

    Facebook login integrated

--- www/app/auth/register.ctrl.js
+++ www/app/auth/register.ctrl.js
@@ -19,12 +19,28 @@
       }
     }
 
-    $scope.register = function() {
-      console.log($scope.user);
+    $scope.register = function(user) {
+      console.log(user);
     }
 
     $scope.loginWithFB = function() {
       console.log("loginWithFB called");
+      facebookConnectPlugin.login(["email"], function(response) {
+        console.log(response);
+        facebookConnectPlugin.api("me?fields=id,name,picture{url},email,first_name,last_name",["public_profile","email"],function(response) {
+          console.log(response);
+          var user = {};
+          user.name = response.name;
+          user.email = response.email;
+
+          $scope.register(user)
+
+        }, function(err) {
+          console.log(err);
+        })
+      },function(err) {
+        console.log(err);
+      })
     }
   }])
 })()
